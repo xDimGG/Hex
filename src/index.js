545d41a61f618eae5bbfce4c1384e215dae0caff
Shayne Har <ShayneEHartford@gmail.Com>
2018-01-22 10:12:23 -0500

    Fixes logger formatting

--- src/index.js
+++ src/index.js
@@ -14,28 +14,31 @@ const client = new AkairoClient({
 	listenerDirectory: `./src/listeners/`
 });
 
-client.console = (input, type) => {
-	client.guilds.get(`361532026354139156`).channels.find(`name`, `console`).send(new MessageEmbed()
-		.setDescription(`\`\`\`\n${input}\n\`\`\``)
-		.setColor(type === `Log` ? 0x00FF00 : 0xFF0000)
-		.setFooter(`${type}`)
-		.setTimestamp()
-	);
-};
-
 client.log = input => {
 	console.log(input);
-	if (!process.env.DEV) client.console(input, `Log`);
+	if (process.env.DEV) return;
+	client.guilds.get(`361532026354139156`).channels.find(`name`, `console`).send(new MessageEmbed()
+		.setDescription(`\`\`\`\n${input}\n\`\`\``)
+		.setColor(0x00FF00)
+	);
 };
 
 client.warn = input => {
 	console.warn(input);
-	if (!process.env.DEV) client.console(input, `Warn`);
+	if (process.env.DEV) return;
+	client.guilds.get(`361532026354139156`).channels.find(`name`, `console`).send(new MessageEmbed()
+		.setDescription(`\`\`\`\n${input}\n\`\`\``)
+		.setColor(0xFFFF00)
+	);
 };
 
 client.error = input => {
 	console.error(input);
-	if (!process.env.DEV) client.console(input, `Error`);
+	if (process.env.DEV) return;
+	client.guilds.get(`361532026354139156`).channels.find(`name`, `console`).send(new MessageEmbed()
+		.setDescription(`\`\`\`\n${input}\n\`\`\``)
+		.setColor(0xFF0000)
+	);
 };
 
 client.login(process.env.Token).then(() => console.log(client.user.tag));
