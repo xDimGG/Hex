d4fe9f62e99e68803941683a4fda1f8de8dcb726
Shayne Har <ShayneEHartford@gmail.Com>
2018-01-23 06:45:47 -0500

    Removed early process events

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 const Client = require(`./structures/Client`);
 new Client().start();
-
-process.on(`uncaughtException`, error => {
-	console.error(error.stack.replace(new RegExp(`${__dirname}/`, `g`), `./`))
-})
-
-process.on(`unhandledRejection`, error => {
-	console.error(error.stack.replace(new RegExp(`${__dirname}/`, `g`), `./`))
-})
