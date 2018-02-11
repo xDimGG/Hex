19f325d915a3e575fc85ecec6e4250d2adf07d38
Shayne Har <ShayneEHartford@gmail.Com>
2018-02-10 19:06:53 -0500

    Updated config command

--- src/commands/utility/config.js
+++ src/commands/utility/config.js
@@ -25,22 +25,20 @@ module.exports = class This extends Command {
 
 	async exec(message, { option, value }) {
 		const config = await message.guild.get()
-		if (!option) {
+		if (!option || !Object.keys(config).includes(option)) {
 			const configNames = Object.keys(config).filter(k => k !== `createdAt` && k !== `updatedAt` && k !== `id`)
 			const longest = configNames.reduce((long, str) => Math.max(long, str.length), 0)
 
 			return message.channel.send(
-				`= Config =\n` +
+				`= Options =\n` +
         `\n` +
-        `${configNames.map(c => `${c.toUpperCase()}${` `.repeat(longest - c.length)} :: ${config[c]}`).sort().join(`\n`)}`,
-				{
-					code: `asciidoc`,
-					split: { prepend: `\`\`\`asciidoc\n`, append: `\`\`\`` },
-				}
+        `${configNames.map(c => `${c}${` `.repeat(longest - c.length)} :: ${config[c]}`).sort().join(`\n`)}\n` +
+				`\n` +
+				`= To set option use "${this.client.akairoOptions.prefix(message)}config (option) (value)" =`,
+				{ code: `asciidoc` }
 			)
 		}
 
-		if (!Object.keys(config).includes(option)) return message.channel.send(`Unknown option`)
 		await message.guild.set({ [option]: value })
 		message.channel.send(`Updated \`${option}\` from \`${config[option]}\` to \`${value}\``)
 	}
