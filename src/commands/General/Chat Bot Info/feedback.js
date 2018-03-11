0bc0bee9ffb5f2b6e31e7a2bd9a14ea001f74fb3
Shayne Har <ShayneEHartford@gmail.Com>
2018-03-11 12:23:10 -0400

    Fixed feedback

--- src/commands/General/Chat Bot Info/feedback.js
+++ src/commands/General/Chat Bot Info/feedback.js
@@ -12,7 +12,7 @@ module.exports = class extends Command {
 	run(message, [feedback]) {
 		(process.env.DEV ? message.channel : this.client.channels.get(`368572194667888646`)).send(
 			`\`${message.author.tag}\`\n` +
-			`\`#${message.channel.name}\` in \`${message.guild.name}\`\n` +
+			`${message.channel.type === `text` ? `\`#${message.channel.name}\` in \`${message.guild.name}\`\n` : `DMs / GroupDMs\n`}` +
 			`\`\`\`\n${feedback}\n\`\`\``
 		).then(async m => {
 			await m.react(`👍`)
