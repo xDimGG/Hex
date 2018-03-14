5f86798dd2869252f6d33244154c9eb6ab3b1381
Shayne Har <ShayneEHartford@gmail.Com>
2018-03-13 21:12:01 -0400

    Removed prefix from help

--- src/commands/General/Chat Bot Info/help.js
+++ src/commands/General/Chat Bot Info/help.js
@@ -53,7 +53,7 @@ module.exports = class extends Command {
 				if (!help.hasOwnProperty(command.category)) help[command.category] = {}
 				if (!help[command.category].hasOwnProperty(command.subCategory)) help[command.category][command.subCategory] = []
 				const description = typeof command.description === `function` ? command.description(msg) : command.description
-				help[command.category][command.subCategory].push(`${msg.guildConfigs.prefix}${command.name.padEnd(longest)} :: ${description}`)
+				help[command.category][command.subCategory].push(`${command.name.padEnd(longest)} :: ${description}`)
 			}).catch(() => {})
 		))
 
