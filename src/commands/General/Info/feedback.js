5044ded7783395f2aec9071fea049ba4edacc2a9
Shayne Har <ShayneEHartford@gmail.Com>
2018-03-20 03:54:20 -0400

    Fixed feedback command

--- src/commands/General/Info/feedback.js
+++ src/commands/General/Info/feedback.js
@@ -4,17 +4,17 @@ module.exports = class extends Command {
 	constructor(...args) {
 		super(...args, {
 			enabled: true,
-			usage: `<Feedback:string>`,
+			usage: `<Feedback:string> [...]`,
 			description: `Sends feedback to the bot creator`,
 			extendedDescription: `Sends your message to the bots support server to be voted upon by everyone else.`,
 		})
 	}
 
-	run(message, [feedback]) {
+	run(message, [...feedback]) {
 		(process.env.DEV ? message.channel : this.client.channels.get(`368572194667888646`)).send(
 			`\`${message.author.tag}\`\n` +
 			`${message.channel.type === `text` ? `\`#${message.channel.name}\` in \`${message.guild.name}\`\n` : `DMs / GroupDMs\n`}` +
-			`\`\`\`\n${feedback}\n\`\`\``
+			`\`\`\`\n${feedback.join(` `)}\n\`\`\``
 		).then(async m => {
 			await m.react(`👍`)
 			await m.react(`👎`)
