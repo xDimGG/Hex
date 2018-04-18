931fcf2ff48dad144efba0d17a43580746d97df2
Shayne Har <ShayneEHartford@gmail.Com>
2018-04-18 03:18:33 -0400

    Added blacklist

---
+++ src/inhibitors/blacklist.js
@@ -0,0 +1,18 @@
+const
+	{ Inhibitor } = require(`discord-akairo`),
+	{ basename } = require(`path`)
+
+module.exports = class extends Inhibitor {
+	constructor() {
+		super(basename(__filename).split(`.`)[0], {
+			reason: basename(__filename).split(`.`)[0],
+			type: `all`,
+		})
+	}
+
+	exec(message) {
+		const blacklist = [`212252550689193985`]
+
+		return blacklist.includes(message.author.id)
+	}
+}
