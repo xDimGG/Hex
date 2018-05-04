d6a24a9d4131505da97d133bc42dc9b74ce736d3
Shayne Har <ShayneEHartford@gmail.Com>
2018-05-04 07:36:39 -0400

    Fixed log function

--- src/structures/Utils.js
+++ src/structures/Utils.js
@@ -1,11 +1,12 @@
+const { isArray, isError, inspect } = require('util');
 const { post } = require('snekfetch');
-const { isError, inspect } = require('util');
 
 module.exports = {
 	log(...content) {
 		console[content instanceof Error ? 'error' : 'log'](...content);
+		if (isArray(content)) content = content.join('\n');
 		if (isError(content)) content = inspect(content, { depth: 10 });
-		if (!process.env.DEV) post(`https://discordapp.com/api/webhooks/${process.env.CONSOLE}`, { data: { avatar_url: 'https://api.shaybox.com/discord/avatar/361796552165031936', ...content, username: 'Hex' } });
+		if (!process.env.DEV) post(`https://discordapp.com/api/webhooks/${process.env.CONSOLE}`, { data: { avatar_url: 'https://api.shaybox.com/discord/avatar/361796552165031936', content, username: 'Hex' } });
 	},
 
 	updateActivity(client) {
