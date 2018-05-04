2c7648040d6e3cce97b926f36682da568adf50a0
Shayne Har <ShayneEHartford@gmail.Com>
2018-05-04 07:28:37 -0400

    Fixed ready

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -2,7 +2,7 @@ const Listener = require('../structures/Listener');
 const { log, updateActivity } = require('../structures/Utils');
 
 module.exports = class extends Listener {
-	exec() {
+	async exec() {
 		log(this.client.user.tag);
 		updateActivity(this.client);
 
