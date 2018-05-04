046b503a075a812bfdcc045a4e8579a71c68bc94
Shayne Har <ShayneEHartford@gmail.Com>
2018-05-04 06:54:22 -0400

    Add ready log

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,8 +1,9 @@
 const Listener = require('../structures/Listener');
-const { updateActivity } = require('../structures/Utils');
+const { log, updateActivity } = require('../structures/Utils');
 
 module.exports = class extends Listener {
-	async exec() {
+	exec() {
+		log(this.client.user.tag);
 		updateActivity(this.client);
 
 		this.client.bannedUsers = await this.client.guilds.get('361532026354139156').fetchBans();
