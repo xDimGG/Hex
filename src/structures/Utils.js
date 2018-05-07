7f1eb8e2a0d9797325b17ae3019b541ff220828b
Shayne Har <ShayneEHartford@gmail.Com>
2018-05-07 06:54:59 -0400

    Null errors from server count updates

--- src/structures/Utils.js
+++ src/structures/Utils.js
@@ -12,8 +12,8 @@ module.exports = {
 	updateActivity(client) {
 		client.user.setActivity(`${client.guilds.size} ${client.guilds.size > 1 ? 'Guilds' : 'Guild'} | ${client.guilds.reduce((a, b) => a + b.memberCount, 0)} Members`, { type: 'WATCHING' });
 		if (!process.env.dev && process.env.dbl_api) {
-			post(`https://discordbots.org/api/bots/${client.user.id}/stats`, { data: { server_count: client.guilds.size }, headers: { Authorization: process.env.dbl_api } }).end();
-			post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`, { data: { server_count: client.guilds.size }, headers: { Authorization: process.env.dbots_api } }).end();
+			post(`https://discordbots.org/api/bots/${client.user.id}/stats`, { data: { server_count: client.guilds.size }, headers: { Authorization: process.env.dbl_api } }).end().catch(() => {});
+			post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`, { data: { server_count: client.guilds.size }, headers: { Authorization: process.env.dbots_api } }).end().catch(() => {});
 		}
 	},
 };
