29e735d5421c398b16c60dd5051bfe6dc2cac5d1
Shayne Har <ShayneEHartford@gmail.Com>
2018-05-07 10:14:17 -0400

    Fixed error event

--- src/listeners/error.js
+++ src/listeners/error.js
@@ -1,12 +1,17 @@
 const Listener = require('../structures/Listener');
 
 module.exports = class extends Listener {
-	exec(error, message) {
+	constructor() {
+		super({ emitter: 'commandHandler' });
+	}
+
+	async exec(error, message) {
 		if (!message) return;
-		message.channel.send([
+		await message.channel.send([
 			'An unexpected error has occured',
 			'Please report this to my support server <https://discord.shaybox.com/>',
 			`\`\`\`${error.stack}\`\`\``,
 		]);
+		await message.channel.stopTyping();
 	}
 };
