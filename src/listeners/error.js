7db0ba10c2261e87d6117a82a6141c728f99dda2
Shayne Har <ShayneEHartford@gmail.Com>
2018-05-07 04:24:32 -0400

    Fixed error event

--- src/listeners/error.js
+++ src/listeners/error.js
@@ -1,8 +1,12 @@
 const Listener = require('../structures/Listener');
-const { inspect } = require('util');
 
 module.exports = class extends Listener {
 	exec(error, message) {
-		message.channel.send(['An unexpected error has occured', 'Please report this to my support server <https://discord.shaybox.com/>', `\`\`\`${inspect(error)}\`\`\``]);
+		if (!message) return;
+		message.channel.send([
+			'An unexpected error has occured',
+			'Please report this to my support server <https://discord.shaybox.com/>',
+			`\`\`\`${error.stack}\`\`\``,
+		]);
 	}
 };
