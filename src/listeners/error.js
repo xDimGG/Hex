1da338642c39c4e4be0a03fc7028140dfb178596
Shayne Har <ShayneEHartford@gmail.Com>
2018-05-09 18:14:25 -0400

    Filter out database errors

--- src/listeners/error.js
+++ src/listeners/error.js
@@ -1,4 +1,6 @@
 const Listener = require('../structures/Listener');
+const { log } = require('../structures/Utils');
+const { BaseError } = require('sequelize');
 
 module.exports = class extends Listener {
 	constructor() {
@@ -6,7 +8,9 @@ module.exports = class extends Listener {
 	}
 
 	async exec(error, message) {
+		log(error);
 		if (!message) return;
+		if (error instanceof BaseError) return;
 		await message.channel.send([
 			'An unexpected error has occured',
 			'Please report this to my support server <https://discord.shaybox.com/>',
