9aa84af840fb9b5caf83a0aca529f5fd9630b3d4
Shayne Har <ShayneEHartford@gmail.Com>
2018-05-19 21:56:56 -0400

    Bot start log

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -5,5 +5,6 @@ module.exports = class extends Listener {
 	exec() {
 		console.log(this.client.user.tag);
 		updateActivity(this.client);
+		this.client.channels.get('361533828520476684').send(this.client.user.tag);
 	}
 };
