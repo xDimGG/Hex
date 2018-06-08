20c3401530b4d9f8cc325acfed947f8f6c5ed2c5
Shayne Har <ShayneEHartford@gmail.Com>
2018-06-08 18:26:16 -0400

    Added DBL server count

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,6 @@
 import { Event } from 'klasa';
 import { TextChannel } from 'discord.js';
+import { post } from 'snekfetch';
 
 export default class extends Event {
 	async run() {
@@ -7,5 +8,14 @@ export default class extends Event {
 
 		const consoleChannel = this.client.channels.get('361533828520476684') as TextChannel;
 		if (consoleChannel) await consoleChannel.send(this.client.user.tag);
+
+		post(`https://discordbots.org/api/bots/${this.client.user.id}/stats`, {
+			data: {
+				server_count: this.client.guilds.size,
+				shard_count: this.client.shard.count,
+				shard_id: this.client.shard.id
+			},
+			headers: { Authorization: process.env.DBL_API }
+		});
 	}
 }
