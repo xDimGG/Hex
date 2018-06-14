9e4195010112b20ee5b95c2893ae4437dd1379dd
Shayne Har <ShayneEHartford@gmail.Com>
2018-06-13 21:31:51 -0400

    Increase shard count to try and decrease delay

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ShardingManager } from 'discord.js';
 
-const manager = new ShardingManager('./src/bot.ts', { token: process.env.TOKEN });
+const manager = new ShardingManager('./src/bot.ts', { totalShards: 4, token: process.env.TOKEN });
 
 // tslint:disable-next-line:no-floating-promises
 manager.spawn();
