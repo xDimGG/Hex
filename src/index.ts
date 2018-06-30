bbae91111278575f6516f86c7152adc0d46f53ed
Shayne Har <ShayneEHartford@gmail.Com>
2018-06-30 10:54:57 -0400

    Temporary fix before rewrite

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { ShardingManager } from 'discord.js';
+import { ShardingManager, Shard } from 'discord.js';
 
 // tslint:disable-next-line:no-floating-promises
-new ShardingManager('./src/client.ts', { token: process.env.TOKEN })
-	.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`))
+new ShardingManager('./build/client.js', { token: process.env.TOKEN, execArgv: process.execArgv })
+	.on('shardCreate', (shard: Shard) => console.log(`Launched shard ${shard.id}`))
 	.spawn();
