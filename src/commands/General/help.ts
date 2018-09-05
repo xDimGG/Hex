39b0a6a9ecc4ba73167bf2542c9f91b32aba510c
ShayBox <shayneehartford@gmail.com>
2018-09-05 04:47:14 -0400

    Updated help

--- src/commands/General/help.ts
+++ src/commands/General/help.ts
@@ -9,13 +9,14 @@ export default class extends Command {
 	}
 
 	public async exec(message: Message) {
-		const allCommands = this.handler.modules.filter(c => c.id !== 'help' && !c.ownerOnly).sort();
+		const commandFilter = command => command.id !== 'help' && !command.ownerOnly;
+		const allCommands = this.handler.modules.filter(commandFilter).sort();
 		const longest = allCommands.keyArray().reduce((long, str) => Math.max(long, str.length), 0);
 		const output: string[] = [];
 
 		this.handler.categories.forEach(category => {
 			output.push(`= ${category.id} =`);
-			category.map(command => {
+			category.filter(commandFilter).forEach(command => {
 				output.push(`${command.id}${' '.repeat(longest - command.id.length)} :: ${command.description}`);
 			});
 			output.push('');
