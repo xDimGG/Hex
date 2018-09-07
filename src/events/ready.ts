8c18b37ce5d06142466cd6e6353441336023f3d4
ShayBox <shayneehartford@gmail.com>
2018-09-06 22:24:26 -0400

    Update shard guild count to be global

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -13,41 +13,30 @@ export default class extends Listener {
 
 		if (process.env.DEV) return;
 
-		const guildCount = (await this.client.shard.broadcastEval('this.guilds.size')).reduce((a: number, b: number) => a + b, 0);
-		await this.client.user.setPresence({
-			activity: {
-				name: `${guildCount} ${guildCount === 0 ? 'Guild' : 'Guilds'}`,
-				type: 'WATCHING',
-				url: 'https://twitch.tv/monstercat',
-			},
-		});
-
 		const { DBL_API, DBOTS_API } = process.env;
 		if (!DBL_API || !DBOTS_API) return console.error('API Keys not set');
 
-		await fetch(`https://discordbots.org/api/bots/${this.client.user.id}/stats`, {
-			body: JSON.stringify({
-				server_count: this.client.guilds.size,
-				shard_count: this.client.shard.count,
-				shard_id: this.client.shard.id,
-			}),
-			headers: {
-				'Authorization': process.env.DBL_API!,
-				'Content-Type': 'application/json',
-			},
-			method: 'POST',
-		});
-		await fetch(`https://bots.discord.pw/api/bots/${this.client.user.id}/stats`, {
-			body: JSON.stringify({
-				server_count: this.client.guilds.size,
-				shard_count: this.client.shard.count,
-				shard_id: this.client.shard.id,
-			}),
-			headers: {
-				'Authorization': process.env.DBOTS_API!,
-				'Content-Type': 'application/json',
-			},
-			method: 'POST',
-		});
+		[
+			[DBL_API, 'https://discordbots.org'],
+			[DBOTS_API, 'https://bots.discord.pw'],
+		].forEach(async ([key, url]) =>
+			fetch(`${url}/api/bots/${this.client.user.id}/stats`, {
+				body: JSON.stringify({
+					server_count: this.client.guilds.size,
+					shard_count: this.client.shard.count,
+					shard_id: this.client.shard.id,
+				}),
+				headers: {
+					'Authorization': key,
+					'Content-Type': 'application/json',
+				},
+				method: 'POST',
+			})
+		);
+
+		if (this.client.shard.id !== this.client.shard.count) return;
+
+		const guildCount = (await this.client.runCommand('this.guilds.size')).reduce((a: number, b: number) => a + b, 0);
+		await this.client.runCommand(`this.user.setPresence({ activity: { name: "${guildCount} ${guildCount === 0 ? 'Guild' : 'Guilds'}", type: "WATCHING" } })`);
 	}
 }
