721c6ee64016b78b0147faa756805f128be3b566
ShayBox <shayneehartford@gmail.com>
2018-09-08 17:53:41 -0400

    Re-added commandLifetime

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -21,6 +21,7 @@ export default class extends AkairoClient implements Client {
 		this.database = new Database(DATABASE!);
 		this.commandHandler = new CommandHandler(this, {
 			automateCategories: true,
+			commandUtilLifetime: 60,
 			directory: './src/commands',
 			handleEdits: true,
 			prefix: async message => message.guild ? message.guild.prefix : 'h!',
