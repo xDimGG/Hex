625b4bfc006fb76d764cbb1367856aff74e3720d
ShayBox <shayneehartford@gmail.com>
2018-09-17 07:19:00 -0400

    Server side logging

--- src/structures/Server.ts
+++ src/structures/Server.ts
@@ -9,7 +9,7 @@ if (!PORT || !DBL_API || !DBL_AUTH) throw new Error('PORT, DBL_API, and/or DBL_A
 
 export default class extends EventEmitter {
 	private server: any;
-	private users: { [index: string]: boolean };
+	private users: { [index: string]: { time: number; voted: boolean } };
 	public constructor() {
 		super();
 		this.users = {};
@@ -17,20 +17,23 @@ export default class extends EventEmitter {
 			.use(bodyParser.json())
 			.get('/user/:id', async (req: ServerRequest, res: ServerResponse) => {
 				if (req.headers.authorization !== DBL_AUTH) return res.end('Not authorized');
+				console.log('Check');
 
 				const userID = (req as any).params.id;
-				if (this.users[userID] === undefined)
-					this.users[userID] = await this.fetch(`/bots/361796552165031936/check?userId=${userID}`)
-						.then(json => json.voted);
+				if (this.users[userID] === undefined || (new Date().getTime() - this.users[userID].time) > 86400000) this.users[userID] = {
+					...await this.fetch(`/bots/361796552165031936/check?userId=${userID}`),
+					time: new Date().getTime() - 82800000,
+				};
 
 				res.writeHead(200, { 'Content-Type': 'application/json' });
 				res.end(JSON.stringify(this.users[userID]));
 			})
 			.post('/webhook', (req: ServerRequest, res: ServerResponse) => {
 				if (req.headers.authorization !== DBL_AUTH) return res.end();
+				console.log('Webhook');
 
 				const body = (req as any).body as { user: string };
-				this.users[body.user] = true;
+				this.users[body.user] = { voted: true, time: new Date().getTime() };
 				res.end();
 			});
 	}
